21f143a146abf11b6b8cfeca6cb0d13664988784
gotenks <sharadtkd123@gmail.com>
2017-07-08 19:40:50 +0530

    changes->login page

--- src/artgallery/LoginPage.java
+++ src/artgallery/LoginPage.java
@@ -15,6 +15,9 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
 //
 //import javax.swing.ButtonGroup;
 //import javax.swing.JLabel;
@@ -30,6 +33,7 @@ public class LoginPage extends JFrame {
 	private JTextField nuser;
 	private JTextField ruser;
 	private JPasswordField rpass;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -52,7 +56,7 @@ public class LoginPage extends JFrame {
 	 */
 	public LoginPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 866, 483);
+		setBounds(100, 100, 731, 445);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		Color c=new Color(216, 186, 186);
@@ -80,125 +84,133 @@ public class LoginPage extends JFrame {
 		mnContactUs.setForeground(Color.WHITE);
 		mnContactUs.setBackground(Color.BLACK);
 		menuBar.add(mnContactUs);
-		contentPane.setBackground(c);
+		contentPane.setBackground(UIManager.getColor("info"));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JRadioButton rdbtnAdmin = new JRadioButton("Adminstrator ");
-		rdbtnAdmin.setBackground(SystemColor.textHighlight);
+		JPanel panel = new JPanel();
+		panel.setBackground(UIManager.getColor("info"));
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel.setBounds(379, 82, 322, 287);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JRadioButton rdbtnAdmin = new JRadioButton("Admin");
+		buttonGroup.add(rdbtnAdmin);
+		rdbtnAdmin.setBounds(146, 152, 90, 33);
+		panel.add(rdbtnAdmin);
+		rdbtnAdmin.setBackground(UIManager.getColor("info"));
 		rdbtnAdmin.setFont(new Font("Berlin Sans FB", Font.PLAIN, 16));
-		rdbtnAdmin.setBounds(436, 283, 115, 33);
-		contentPane.add(rdbtnAdmin);
-		
-		JLabel lblNewLabel = new JLabel("E-mail :");
-		lblNewLabel.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		lblNewLabel.setBounds(35, 247, 101, 27);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Password :");
-		lblNewLabel_1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(35, 285, 91, 27);
-		contentPane.add(lblNewLabel_1);
-		
-		nemail = new JTextField();
-		nemail.setBounds(166, 253, 101, 20);
-		contentPane.add(nemail);
-		nemail.setColumns(10);
-		
-		JLabel lblTypeOfUser = new JLabel("Type of User : ");
-		lblTypeOfUser.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		lblTypeOfUser.setBounds(436, 250, 126, 20);
-		contentPane.add(lblTypeOfUser);
 		
 		JRadioButton rdbtnUser = new JRadioButton("User");
-		rdbtnUser.setBackground(SystemColor.textHighlight);
+		buttonGroup.add(rdbtnUser);
+		rdbtnUser.setBounds(240, 154, 76, 29);
+		panel.add(rdbtnUser);
+		rdbtnUser.setSelected(true);
+		rdbtnUser.setBackground(UIManager.getColor("info"));
 		rdbtnUser.setFont(new Font("Berlin Sans FB", Font.PLAIN, 16));
-		rdbtnUser.setBounds(588, 285, 115, 29);
-		contentPane.add(rdbtnUser);
 		
-		ButtonGroup group=new ButtonGroup();
-		group.add(rdbtnAdmin);
-		group.add(rdbtnUser);
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(5, 189, 303, 33);
+		panel.add(btnLogin);
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel label = new JLabel("Username :");
+		label.setBounds(5, 78, 157, 27);
+		panel.add(label);
+		label.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
+		
+		JLabel label_1 = new JLabel("Password :");
+		label_1.setBounds(5, 117, 159, 27);
+		panel.add(label_1);
+		label_1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
+		
+		ruser = new JTextField();
+		ruser.setBounds(182, 83, 126, 20);
+		panel.add(ruser);
+		ruser.setColumns(10);
+		
+		rpass = new JPasswordField();
+		rpass.setBounds(182, 122, 126, 20);
+		panel.add(rpass);
+		
+		JLabel lblNewLabel_2 = new JLabel("Registered User");
+		lblNewLabel_2.setBounds(8, 17, 212, 39);
+		panel.add(lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("Segoe UI Semilight", Font.BOLD, 19));
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(5, 56, 311, 13);
+		panel.add(separator);
+		
+		JPanel registerpanel = new JPanel();
+		registerpanel.setBackground(UIManager.getColor("info"));
+		registerpanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		registerpanel.setBounds(15, 84, 309, 287);
+		contentPane.add(registerpanel);
+		registerpanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("E-mail :");
+		lblNewLabel.setBounds(5, 158, 155, 27);
+		registerpanel.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
+		
+		JLabel lblNewLabel_1 = new JLabel("Password :");
+		lblNewLabel_1.setBounds(5, 196, 155, 27);
+		registerpanel.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
+		
+		nemail = new JTextField();
+		nemail.setBounds(178, 169, 126, 20);
+		registerpanel.add(nemail);
+		nemail.setColumns(10);
 		
 		npass = new JPasswordField();
-		npass.setBounds(166, 291, 101, 20);
-		contentPane.add(npass);
+		npass.setBounds(178, 207, 126, 20);
+		registerpanel.add(npass);
 		
 		JLabel lblEmail = new JLabel("Username :");
+		lblEmail.setBounds(5, 120, 155, 27);
+		registerpanel.add(lblEmail);
 		lblEmail.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		lblEmail.setBounds(35, 209, 101, 27);
-		contentPane.add(lblEmail);
 		
 		JLabel lblName = new JLabel("Name :");
+		lblName.setBounds(5, 82, 155, 27);
+		registerpanel.add(lblName);
 		lblName.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		lblName.setBounds(35, 171, 101, 27);
-		contentPane.add(lblName);
 		
 		nname = new JTextField();
-		nname.setBounds(166, 177, 101, 20);
-		contentPane.add(nname);
+		nname.setBounds(178, 93, 126, 20);
+		registerpanel.add(nname);
 		nname.setColumns(10);
 		
 		nuser = new JTextField();
-		nuser.setBounds(166, 215, 101, 20);
-		contentPane.add(nuser);
+		nuser.setBounds(178, 131, 126, 20);
+		registerpanel.add(nuser);
 		nuser.setColumns(10);
 		
 		JButton btnRegister = new JButton("Register");
+		btnRegister.setBounds(5, 239, 299, 33);
+		registerpanel.add(btnRegister);
 		btnRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
 		btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnRegister.setBounds(101, 338, 101, 33);
-		contentPane.add(btnRegister);
 		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnLogin.setBounds(534, 338, 101, 33);
-		contentPane.add(btnLogin);
-		
-		JLabel label = new JLabel("Username :");
-		label.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		label.setBounds(436, 141, 101, 27);
-		contentPane.add(label);
-		
-		JLabel label_1 = new JLabel("Password :");
-		label_1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
-		label_1.setBounds(436, 195, 91, 27);
-		contentPane.add(label_1);
-		
-		ruser = new JTextField();
-		ruser.setColumns(10);
-		ruser.setBounds(547, 147, 101, 20);
-		contentPane.add(ruser);
-		
-		rpass = new JPasswordField();
-		rpass.setBounds(547, 195, 101, 20);
-		contentPane.add(rpass);
-		
-		JLabel lblNewLabel_2 = new JLabel("Registered User");
-		lblNewLabel_2.setFont(new Font("Segoe UI Semilight", Font.BOLD, 19));
-		lblNewLabel_2.setBounds(462, 54, 212, 39);
-		contentPane.add(lblNewLabel_2);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(462, 106, 164, 2);
-		contentPane.add(separator);
-		
-		JLabel lblNewLanewbel = new JLabel("New User... ?");
+		JLabel lblNewLanewbel = new JLabel("Need an Account?");
+		lblNewLanewbel.setBounds(5, 17, 185, 27);
+		registerpanel.add(lblNewLanewbel);
 		lblNewLanewbel.setFont(new Font("Segoe UI Symbol", Font.BOLD, 18));
-		lblNewLanewbel.setBounds(81, 106, 185, 27);
-		contentPane.add(lblNewLanewbel);
 		
 		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(81, 146, 111, 2);
-		contentPane.add(separator_1);
+		separator_1.setBounds(5, 52, 299, 2);
+		registerpanel.add(separator_1);
 		
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setOrientation(SwingConstants.VERTICAL);
 		separator_2.setForeground(Color.BLACK);
-		separator_2.setBounds(346, 43, 2, 328);
+		separator_2.setBounds(350, 84, 2, 287);
 		contentPane.add(separator_2);
 	}
 }
